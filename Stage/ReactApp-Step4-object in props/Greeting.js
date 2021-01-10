2ae63eed8f9a5059bde2776475867499f58b61be
GES005 <geetha5.subramanian@gmail.com>
2021-01-10 11:48:40 +0530

    Signed-off-by: GES005 <geetha5.subramanian@gmail.com>

---
+++ Stage/ReactApp-Step4-object in props/Greeting.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+export const Greeting = ({person,ToDo}) =>{ 
+  const {name,country}=person;
+  let newDate = new Date(); 
+  let hrs =newDate.getHours();
+  let greeting=(hrs<13);
+  let timeofElt =greeting?<h3>Good Morning {name}</h3>:<h3>Good Evening {name}</h3>;
+  let mins=newDate.getMinutes();
+  let secs=newDate.getSeconds();
+  let date = newDate.getDate();
+  let month = newDate.getMonth() + 1;
+  let year = newDate.getFullYear();
+return  ( <> {timeofElt} 
+<h3>Date is : {date}:{month}:{year} {hrs}:{mins}:{secs} in {country}</h3>
+<h3> Your action item for today {ToDo}</h3></>
+)
+} 
