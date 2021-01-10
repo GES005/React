755390482ad385fcdf3b0d840f9f6c5b8206789e
GES005 <geetha5.subramanian@gmail.com>
2021-01-10 11:16:48 +0530

    Signed-off-by: GES005 <geetha5.subramanian@gmail.com>

---
+++ Stage/ReactApp-Step2-props/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import {Greeting} from './Greeting';
+function App() { 
+  let url="https://reactjs.org";
+   return (
+    <div className="App">
+      <header className="App-header">
+    
+      <Greeting name="Geetha" ToDo="Java"/>
+         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World !!!
+        </p>
+        <a
+          className="App-link"
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
